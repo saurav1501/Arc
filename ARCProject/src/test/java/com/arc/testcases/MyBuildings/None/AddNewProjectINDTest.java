6d2b79ed704bda1d0b93ccbb7d28eea44db4b35e
Group10 <Group10>
2017-11-30 15:14:26 +0530

    fixed dependency

--- ARCProject/src/test/java/com/arc/testcases/MyBuildings/None/AddNewProjectINDTest.java
+++ ARCProject/src/test/java/com/arc/testcases/MyBuildings/None/AddNewProjectINDTest.java
@@ -14,7 +14,7 @@ import com.arc.driver.CommonMethod;
 
 public class AddNewProjectINDTest extends BaseClass {
 	
-	@Test(dependsOnMethods={"com.arc.testcases.MyBuildings.None.LoginCaseTest.loginCaseTest"})
+	@Test(dependsOnMethods={"com.arc.testcases.MyBuildings.None.LoginCaseTest.loginCase"})
 	@Parameters({"rowNum" ,"loginSheet","buildingSheet"})
 	public void addNewProjectTest(int rowNum, String loginSheet, String buildingSheet ) throws IOException {
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
