3be89e36da4d71eba161dff6cde129dee4f83861
Group10 <Group10>
2017-11-30 15:37:10 +0530

    fixed dependency

--- ARCProject/src/test/java/com/arc/testcases/MyBuildings/None/TeamMemberCreditAssertionTest.java
+++ ARCProject/src/test/java/com/arc/testcases/MyBuildings/None/TeamMemberCreditAssertionTest.java
@@ -16,7 +16,7 @@ import com.arc.driver.CommonMethod;
 public class TeamMemberCreditAssertionTest extends BaseClass {
 
 	
-	@Test(dependsOnMethods = { "com.arc.testcases.MyBuildings.None.Login.LoginCaseTest.loginCaseTest","com.arc.testcases.MyBuildings.None.PublicSearch.SearchProgramTest.searchProgramTest","com.arc.testcases.MyBuildings.None.PublicSearch.ClickSearchedProgramTest.clickSearchedProgramTest","com.arc.testcases.MyBuildings.None.AddProject.PaymentbyCCTest.paymentbyCCTest" })
+	@Test(dependsOnMethods = { "com.arc.testcases.MyBuildings.None.LoginCaseTest.loginCase","com.arc.testcases.MyBuildings.None.SearchProgramTest.searchProgram","com.arc.testcases.MyBuildings.None.ClickSearchedProgramTest.clickSearchedProgram","com.arc.testcases.MyBuildings.None.PaymentbyCCTest.paymentbyCC" })
 	@Parameters({"rowNum" ,"buildingSheet","loginSheet"})
 	public void prerequisitesAttempt(int rowNum, String buildingSheet, String loginSheet) throws IOException {
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
